60b074f4e4d644f0222c77e49164b390f10c626a
Carlos Henrico <143365504+henric00@users.noreply.github.com>
2025-04-08 20:20:25 -0300

    função que inicia o jogo

--- javascripit/jogo.js
+++ javascripit/jogo.js
@@ -1,19 +1,19 @@
-/*const inputJogador = document.getElementById("entradaJogador");
+const inputJogador = document.getElementById("entradaJogador");
 const painelPalavra = document.getElementById("painelPalavra");
 const painelFeedback = document.getElementById("painelFeedback");
 const painelPontos = document.getElementById("painelPontos");
-const painelTempo = document.getElementById("painelTempo");*
+const painelTempo = document.getElementById("painelTempo");
 
 //lista das palavras que vão aparecer no jogo
 const palavras =["matrix", "neon", "pixel"," programação", "funcional", "codigo", "bit", "nave", "bug", "byte", "terminal"]
 
 //como o jogo vai começar
-/*const estadoInicial = {
+const estadoInicial = {
     pontos: 0,
     tempo: 30, 
     palavraAtual: "",
-    palavrasRestantes: [...palavras] 
-}*/
+    palavrasRestantes: [...palavras]
+}
 
 //pegando uma palavra aleatoria e diminuindo a lista(imprime uma copia sem o indice que foi escolhido)
 const escolherPalavra = (estado) => {
@@ -25,20 +25,14 @@ const escolherPalavra = (estado) => {
         ...estado, 
         palavraAtual: palavra.trim(),
         palavrasRestantes: novasPalavras
-    }
+    };
     
-}
-// Teste rápido da função sortearPalavra
-const testeEstado = {
-    pontos: 0,
-    tempo: 30,
-    palavraAtual: "",
-    palavrasRestantes: ["matrix", "bug", "neon", "byte"]
-  };
-  
-  const resultado = escolherPalavra(testeEstado);
-  
-  console.log("Palavra sorteada:", resultado.palavraAtual);
-  console.log("Palavras restantes:", resultado.palavrasRestantes);
-  
+};
+
+const iniciarJogo = () => {
+    let estado = escolherPalavra(estadoInicial);
+    painelPalavra.textContent = estado.palavraAtual;
+    painelPontos.textContent = `Pontos: ${estado.pontos}`;
+    painelTempo.textContent = `Tempo: ${estado.tempo}s`;
+};
 
