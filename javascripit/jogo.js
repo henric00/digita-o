342f7308afc87b9da2863a71f6afc5cf62e8866c
Carlos Henrico <143365504+henric00@users.noreply.github.com>
2025-04-08 17:21:00 -0300

    criada a lista de palavras

---
+++ javascripit/jogo.js
@@ -0,0 +1,7 @@
+const inputJogador = document.getElementById("entradaJogador");
+const painelPalavra = document.getElementById("painelPalavra");
+const painelFeedback = document.getElementById("painelFeedback");
+const painelPontos = document.getElementById("painelPontos");
+const painelTempo = document.getElementById("painelTempo");
+
+const palavras =["matrix", "neon", "pixel"," programação", "funcional", "codigo", "bit", "nave", "bug", "byte", "terminal"]
